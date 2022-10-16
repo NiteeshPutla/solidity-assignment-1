f32c0aa5d0b318231f8c39675fc4ca230bea83d8
Niteesh Putla <90021910+NiteeshPutla@users.noreply.github.com>
2022-10-16 17:15:49 +0530

    Create Hi.sol
    Getting the name and twice of age when deployed

---
+++ Hi.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity 0.8.10;
+
+contract Hi {
+    string name;
+    uint8 age;
+
+	constructor(string memory _name, uint8 _age)  {
+		name = _name;
+         age =_age;
+	}
+
+	function getName() external view returns(string memory) {
+		return name;
+	}
+
+	function setName(string memory _name) external {
+		name = _name;
+	}
+     function getAge() external view returns(uint8) {
+	 	return age*2;
+	 }
+
+	 function setAge(uint8 _age) external {
+	 	age = _age;
+	}
+}
